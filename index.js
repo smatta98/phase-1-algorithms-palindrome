792f09615714ff2fba06cab739a9482fe1a7ce11
Shiv Matta <shiv.matta@gmail.com>
2022-07-20 20:23:55 -0500

    commit tree 6b6c26f94383cc9a3b6f45cc646795a683f9c12b

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+let wordLength = word.length
+
+for(let i = 0; i < (wordLength/2); i++) {
+    let character = word[i];
+    let mirrorCharacter = word[wordLength - i -1]
+    console.log(character,mirrorCharacter)
+
+    if (character !== mirrorCharacter) {
+      return false;
+    }
+  
+}
+    return true;
 }
 
-/* 
+console.log(isPalindrome("robot"))
+// function palindromeTwo(string) {
+//   let arrayOriginal = [...string]
+// }
+
+/* l
   Add your pseudocode here
-*/
+  
+  Must take a string then turn it into an array and then iterate through the array and compare the original array to new array mirror image 
+  if every index is equal then it must return true
+
+
+  for each character in "first half of word":
+    if character != mirror_character:
+      return False
+  return True
+
+*/ 
 
 /*
   Add written explanation of your solution here
